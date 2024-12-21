7b64148641f0429eb4fc02a68a30993d98fb951d
George Barh <georgios.barchampas@students.codeinstitute.net>
2024-12-21 19:28:07 +0000

    Fix the links using url_for method

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -13,17 +14,19 @@ def index():
 def about():
     return render_template("about.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
-@app.route("/careers") 
+
+@app.route("/careers")
 def careers():
-       return render_template("careers.html")
+    return render_template("careers.html")
+
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT","5000")),
-        debug= True
-    )
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
